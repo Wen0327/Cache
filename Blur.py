1a0e888637106f0949fe336309e49d227dcce02d
edison.chen <edison.chen@prophetdigits.com>
2024-09-06 11:47:41 +0800

    style:add formatery

--- Blur.py
+++ Blur.py
@@ -70,18 +70,7 @@ async def on_message(message):
         user_played_urls[user_id] = set()
 
     if message.content.lower() == '!help':
-        await message.channel.send(f"** COMMAND LIST **")
-        await message.channel.send(f"!fuckme" )
-        await message.channel.send(f" ")
-        await message.channel.send(f"Users can set difficulty, difficulty range: From 1-10")
-        await message.channel.send(f"!start  <difficulty|Integer>")
-        await message.channel.send(f" ")
-        await message.channel.send(f"!re  <difficulty|Integer>")
-        await message.channel.send(f"Users can reset difficulty with same image")
-        await message.channel.send(f" ")
-        await message.channel.send(f"!giveup")
-        await message.channel.send(f"Users can input this command to show the answer")
-
+        await message.channel.send(f"** COMMAND LIST ** \n  \n" f"!fuckme \n \n " f"Users can set difficulty, difficulty range: From 1-10 \n" f"!start  <difficulty | Integer> \n" f"!re  <difficulty | Integer> \n" f"Users can reset difficulty with same image \n \n" f"!giveup \n" f"Users can input this command to show the answer \n")
 
     if message.content.lower() == '!fuckme':
         user = message.author
