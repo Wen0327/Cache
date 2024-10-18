6d2ef087b674b908f7cf0d155358e122f7d1acfe
edison.chen <edison.chen@prophetdigits.com>
2024-10-18 19:45:35 +0800

    pref: download numbers

--- Scraping.py
+++ Scraping.py
@@ -3,33 +3,34 @@ import requests
 from bs4 import BeautifulSoup
 
 # 設定圖片下載的目標資料夾
-
-# downloaded_Char downloaded_Ball
+# switch to character
+# downloaded_Char
+# switch to ball
+# downloaded_Ball
 download_folder = os.path.expanduser("~/Downloads/Side Project/Cache/downloaded_Char")
 if not os.path.exists(download_folder):
     os.makedirs(download_folder)
 
 # 從6199開始到6551的範圍，逐個下載
-start_number = 6199
-end_number = 6551
+start_number = 6100
+end_number = 6600
 
-# 儲存檔名從1.png開始
-image_number = 1
 
 # 遍歷範圍，下載圖片
 for random_number in range(start_number, end_number + 1):
     url = f"https://dic.xflag.com/monsterstrike/assets-update/img/monster/{random_number}/character.webp"
+
     # switch to character
     # f"https://dic.xflag.com/monsterstrike/assets-update/img/monster/{random_number}/character.webp"
     # switch to ball
     # f"https://dic.xflag.com/monsterstrike/assets-update/img/monster/{random_number}/ball.webp"
 
+    # 儲存檔名與number相同
     # 檢查圖片是否已經存在
-    img_name = f"{image_number}.png"
+    img_name = f"{random_number}.png"
     img_path = os.path.join(download_folder, img_name)
     if os.path.exists(img_path):
         print(f"圖片已存在，跳過: {img_name}")
-        image_number += 1  # 檔名遞增
         continue
 
     try:
@@ -40,13 +41,13 @@ for random_number in range(start_number, end_number + 1):
             img_data = response.content
 
             # 儲存圖片，檔名依次從5.png開始
-            img_name = f"{image_number}.png"
+            img_name = f"{random_number}.png"
             img_path = os.path.join(download_folder, img_name)
             with open(img_path, "wb") as handler:
                 handler.write(img_data)
 
             print(f"圖片已下載: {img_name}")
-            image_number += 1  # 檔名遞增
+
         else:
             print(f"圖片不存在，跳過: {random_number}")
 
